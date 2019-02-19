f6aa35b0537f706442dde5212e7e67b7f3611152
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2019-02-19 10:02:49 +0700

    Fix create build directory

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 const createFile = (name, content) => {
+  if (!fs.existsSync(path.join(process.cwd(), 'build'))) {
+    fs.mkdirSync(path.join(process.cwd(), 'build'))
+  }
+
   const fileName = uniqid(`${name}-`)
   const file = path.join(process.cwd(), 'build', fileName)
 
