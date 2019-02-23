7c4cf688ce87fcd08e250a7c54d12709d46d44cd
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2019-02-23 15:45:18 +0700

    Latest push from local

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 const execute = require('promise-exec')
@@ -7,8 +8,8 @@ const template = require('./template')
 const uniqid = require('uniqid')
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(bodyParser.json())
 
 const createFile = (name, content) => {
